351874a16b21667de87ddbc221263a16b2b5c3af
ちゃーん (I-Jang) <jang_inhwa@microad.co.jp>
2017-04-09 20:58:45 +0900

    4月9日, priorityぬるぽん

--- src/fe/afternoon/h28h/ToDoList.java
+++ src/fe/afternoon/h28h/ToDoList.java
@@ -15,25 +15,27 @@ public class ToDoList {
 	public void update(ToDo todo) {
 		int index = todoList.indexOf(todo);
 		/*
-		 * 指定された要素がこのリスト内で最初に検出された位置のインデックスを返します。
-		 * 指定された要素がこのリストにない場合は -1を返します。
+		 * 指定された要素がこのリスト内で最初に検出された位置のインデックスを返します。 指定された要素がこのリストにない場合は -1を返します。
 		 */
-		if(index != -1){
+		if (index != -1) {
 			todoList.set(index, todo);
 		}
 	}
-	
-	public List<ToDo> select(Condition...conditions){
+
+	public List<ToDo> select(Condition... conditions) {
 		List<ToDo> result = new ArrayList<>();
-		for(ToDo todo : todoList){
+		for (ToDo todo : todoList) {
 			boolean selected = true;
-			for(Condition condition : conditions){
-				selected = condition.test(todo);
-				
-				if(!selected)
+			for (Condition condition : conditions) {
+				selected = condition.test(todo); // Debugの結果、優先度がなぜかnull・・・・・。
+
+				if (!selected)
 					break;
 			}
+			if (selected) {
+				result.add(new ToDo(todo));
+			}
 		}
-		return result;	
-	}	
+		return result;
+	}
 }
