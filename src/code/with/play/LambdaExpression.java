d9c84659f532a97a56a4d58c69e79ac1f5556e60
jang_inhwa <jang_inhwa@192.168.123.105>
2017-04-19 23:02:18 +0900

    0419, 全然わからない

--- src/code/with/play/LambdaExpression.java
+++ src/code/with/play/LambdaExpression.java
@@ -1,17 +1,36 @@
 package code.with.play;
 
+import static java.lang.System.*;
+
 import java.util.function.IntBinaryOperator;
 
 public class LambdaExpression {
 
 	public static void main(String[] args) {
 
+		// --1--//
 		IntBinaryOperator func = (int a, int b) -> {
 			return a - b;
 		};
 
 		int a = func.applyAsInt(5, 3);
-		System.out.println(a);
+		out.println(a);
+
+		// --2--//
+
+		out.println(new java.util.Date());
+		LambdaExpressionTest test = new LambdaExpressionTest();
+		test.setId(18);
+		test.getList().add(18);
+
+		// --3--//
+		// アカウントの有効無効を判定 (RelayAdminUserProductのproductIdが６の要素が存在するか探索)
+		//boolean accountEnable = userProductList.stream()
+		//		.anyMatch(userProduct -> new Integer(6).equals(userProduct.getProductId()));
+
+
+
+
 	}
 
 }
---
+++ src/code/with/play/LambdaExpressionTest.java
@@ -0,0 +1,43 @@
+package code.with.play;
+
+import java.util.List;
+
+public class LambdaExpressionTest {
+
+	private String str;
+	private byte[] b;
+	private List<Integer> list;
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSrt() {
+		return str;
+	}
+
+	public byte[] getByte() {
+		return b;
+	}
+
+	public List<Integer> getList() {
+		return list;
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
+
+	public void setB(byte[] b) {
+		this.b = b;
+	}
+
+	public void setList(List<Integer> list) {
+		this.list = list;
+	}
+}
