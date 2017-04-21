354986948f247f35023273b9c12bfd225e054f2c
jang_inhwa <jang_inhwa@192.168.113.233>
2017-04-21 10:37:55 +0900

    0421, なんとか覚えたい

--- src/code/with/play/LambdaExpression.java
+++ src/code/with/play/LambdaExpression.java
@@ -2,13 +2,16 @@ package code.with.play;
 
 import static java.lang.System.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.IntBinaryOperator;
 
 public class LambdaExpression {
 
 	public static void main(String[] args) {
 
-		// --1--//
+		// --1-- //
+		// int 省略OK
 		IntBinaryOperator func = (int a, int b) -> {
 			return a - b;
 		};
@@ -16,20 +19,44 @@ public class LambdaExpression {
 		int a = func.applyAsInt(5, 3);
 		out.println(a);
 
-		// --2--//
+
+		// --2-- //
 
 		out.println(new java.util.Date());
-		LambdaExpressionTest test = new LambdaExpressionTest();
-		test.setId(18);
-		test.getList().add(18);
+//		LambdaExpressionTest test = new LambdaExpressionTest();
+//		test.getList().add(3333);
+//		test.getList().add(333);
+		out.print(getProperty("line.separator"));
 
-		// --3--//
+
+
+		// --3-- //
+
+
+		List<Integer> test = new ArrayList<>();
+		test.add(333); test.add(3333);
+
+
+		///(1)///
+		test.forEach(out::println);
+
+
+		///(2)///
+		test.forEach((rlt) -> {out.println(rlt);});
+
+
+		///(3)///
+		for(Integer i : test){	out.println(i); }
+
+
+
+
+
+		// --4-- //
 		// アカウントの有効無効を判定 (RelayAdminUserProductのproductIdが６の要素が存在するか探索)
-		//boolean accountEnable = userProductList.stream()
-		//		.anyMatch(userProduct -> new Integer(6).equals(userProduct.getProductId()));
-
-
-
+		// boolean accountEnable = userProductList.stream()
+		// .anyMatch(userProduct -> new
+		// Integer(6).equals(userProduct.getProductId()));
 
 	}
 
