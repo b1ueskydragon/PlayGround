44e695866b36ef36eed1e2c1de4d3032e23c0f31
jang_inhwa <jang_inhwa@192.168.123.105>
2017-04-30 09:52:18 +0900

    0430, 入力と同時にカラー設定するように変えたい

--- src/sevenTokage/tokages/NijiiroTokage.java
+++ src/sevenTokage/tokages/NijiiroTokage.java
@@ -1,6 +1,8 @@
 package sevenTokage.tokages;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 import sevenTokage.attributes.Color;
 
@@ -11,8 +13,37 @@ import sevenTokage.attributes.Color;
  */
 public abstract class NijiiroTokage extends Tokage {
 
-	List<Color> nijiiro ;
+	List<Color> colorList = color.colorList();
+	Scanner sc = null;
 
+	public int inputInt() {
 
+		System.out.println("0から10まで、今日は何番の気分?");
+		int thisOne = 0;
+
+		while (true) {
+			try {
+				sc = new Scanner(System.in);
+				thisOne = sc.nextInt();
+				if (thisOne <= colorList.size())
+					break;
+				else{
+					System.out.print("List Size超えてるわ");
+					continue;
+					}
+			} catch (NoSuchElementException e) {
+				System.out.print("おこ--");
+				continue;
+			}
+		}
+
+		sc.close();
+
+		return thisOne;
+	}
+
+	public void setNumGetColor(int i) {
+		colorList.get(i);
+	}
 
 }
