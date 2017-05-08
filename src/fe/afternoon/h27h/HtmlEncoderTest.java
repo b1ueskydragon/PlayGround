fc7e8d3dd52f7cd954ca86a990e0128094278f61
jang_inhwa <jang_inhwa@192.168.220.140>
2017-05-08 18:22:17 +0900

    0508 まだ未解決

--- src/fe/afternoon/h27h/HtmlEncoderTest.java
+++ src/fe/afternoon/h27h/HtmlEncoderTest.java
@@ -3,5 +3,6 @@ package fe.afternoon.h27h;
 public class HtmlEncoderTest {
 	static public void main(String[] args) {
 	//	new HtmlEncoder().encode("<script>alert('注意!');</script>");
+		// encodeメソッドはなんかい実行される?
 	}
 }
