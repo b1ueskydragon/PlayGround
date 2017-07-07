3b29c9bd0fb1181e7fe5733e7ec99d44706ad492
ijrd1 <jang_inhwa@microad.co.jp>
2017-07-07 22:49:10 +0900

    これが

--- src/code/with/play/HappyTanabata.java
+++ src/code/with/play/HappyTanabata.java
@@ -17,44 +17,52 @@ public class HappyTanabata {
 		return false;
 	}
 
+
 	private static void swap(String[] target, int idx1, int idx2) {
 		String box = target[idx1];
 		target[idx1] = target[idx2];
 		target[idx2] = box;
 	}
 
-	private static void swapMore(String[] targetAry, int i) {
-		// base case
-		int targetIdx = i;
-		String correct = new Character(tanabata.toCharArray()[i]).toString();
-		System.out.println(i);
-
-		// recursive case
-		do {
-			int r = targetIdx + 1;
-			swap(targetAry, targetIdx, r);
-			r++;
+	private static void swapMore(String[] targetAry, int i, int targetIdx, String correct) {
+		while (i < targetAry.length) {
+			swap(targetAry, targetIdx, i);
+			i++;
 			if (targetAry[targetIdx].equals(correct)) {
 				break;
 			}
-		} while (targetIdx + 1 < targetAry.length);
-
-		swapMore(targetAry, i++);
-
+		}
 	}
 
 	public static void main(String[] args) {
 
-		String[] targetAry = { SEVEN, TWO, SEVEN, ONE, ZERO, ZERO, ZERO, SEVEN };
+		String[] targetAry = { SEVEN, SEVEN, SEVEN, ZERO, ZERO, ZERO, TWO, ONE };
 
-		swapMore(targetAry, 0);
+
+		swapMore(targetAry, 0, 0, new Character(tanabata.toCharArray()[0]).toString());
+		swapMore(targetAry, 1, 1, new Character(tanabata.toCharArray()[1]).toString());
+		swapMore(targetAry, 2, 2, new Character(tanabata.toCharArray()[2]).toString());
+		swapMore(targetAry, 3, 3, new Character(tanabata.toCharArray()[3]).toString());
+		swapMore(targetAry, 4, 4, new Character(tanabata.toCharArray()[4]).toString());
+		swapMore(targetAry, 5, 5, new Character(tanabata.toCharArray()[5]).toString());
+		swapMore(targetAry, 6, 6, new Character(tanabata.toCharArray()[6]).toString());
+		swapMore(targetAry, 7, 7, new Character(tanabata.toCharArray()[7]).toString());
+
+		int r = 1;
+		while (r < targetAry.length) {
+			swap(targetAry, 1, r);
+			r++;
+			if (targetAry[1].equals(ZERO)) {
+				r = 0;
+				break;
+			}
+		}
 
 		String target = "";
 		for (String el : targetAry) {
 			target += el;
 		}
 
-		System.out.println(target);
 
 		wellIsTanabata(target);
 
