bbc0e7e53e33128b888a8e5178056806a435d455
ijrd1 <jang_inhwa@microad.co.jp>
2017-07-09 18:38:43 +0900

    満点とりたい

---
+++ src/programmer/se7/java/Primes.java
@@ -0,0 +1,17 @@
+package programmer.se7.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Primes {
+	public static void main(String[] args) {
+		List p = new ArrayList(); // 引数が必要なraw型
+		p.add(7);
+		p.add(2);
+		p.add(5);
+		p.add(2); // ここまではOK printlnも問題なし
+		//p.sort(); // List.sortは引数 () に適用できません
+		System.out.println(p);
+	}
+
+}
