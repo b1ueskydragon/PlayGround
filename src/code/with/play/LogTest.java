6194c446f4e3742ebd638eb96f8b5c16af383d8a
ijrd1 <jang_inhwa@microad.co.jp>
2017-07-14 09:30:01 +0900

    javaの深い世界

---
+++ src/code/with/play/LogTest.java
@@ -0,0 +1,9 @@
+package code.with.play;
+
+public class LogTest {
+
+	public static void main(String[] args) {
+	//	Log logger = LogFactory
+	}
+
+}
---
+++ src/code/with/play/Main.java
@@ -0,0 +1,21 @@
+package code.with.play;
+
+/**
+ * 単にクラスを二つ書いただけ(依存関係)
+ */
+
+class Account {
+	String owner;
+	public Account (String owner){
+		this.owner = owner;
+	}
+
+}
+
+public class Main {
+	public static void main(String [] args) {
+		Account a = new Account("ijrd1");
+		System.out.println(a.owner);
+	}
+
+}
