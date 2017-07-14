058edceac239a1c604532ef8add21b9c80c3f475
ijrd1 <jang_inhwa@microad.co.jp>
2017-07-14 09:39:49 +0900

    あとでやってみよー

---
+++ src/code/with/play/ProcessBuilderTest.java
@@ -0,0 +1,13 @@
+package code.with.play;
+
+public class ProcessBuilderTest {
+
+	public static void main(String [] args) throws Exception {
+
+		ProcessBuilder pb = new ProcessBuilder(
+				"/実行したいファイルのパス/",
+				"/書き込みたいtxtのファイル名/"
+				);
+		pb.start(); // 起動
+	}
+}
