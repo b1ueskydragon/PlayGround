c6cc30b114a4b51b151b2d1d76e8cc2cc85d57cd
jang <jang_inhwa@microad.co.jp>
2017-09-19 16:39:07 +0900

    null set しても問題ない

---
+++ src/code/with/play/NullTest.java
@@ -0,0 +1,22 @@
+package code.with.play;
+
+public class NullTest {
+  public static void main(String[]args){
+    Sample sample = new Sample();
+    sample.setValue(null);
+
+    System.out.println(sample.getValue());
+  }
+}
+
+class Sample{
+  private String value;
+
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+}
