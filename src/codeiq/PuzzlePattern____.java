97dbbe2bbb7452c2a20d3fdbc223e4c4bd31e62b
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-09-20 00:53:53 +0900

    0がまずかったかー

---
+++ src/codeiq/PuzzlePattern____.java
@@ -0,0 +1,13 @@
+package codeiq;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class PuzzlePattern____ {
+
+  public static void main(String[] args) {
+
+  }
+}
