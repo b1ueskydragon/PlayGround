816ca23d2e2526a4d3aefd11746d0b85c32f736a
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-10-16 09:59:39 +0900

    171016

---
+++ src/quiz/OptionalChallenge.java
@@ -0,0 +1,34 @@
+package quiz;
+
+import java.util.Optional;
+
+public class OptionalChallenge {
+
+  public static void main(String [] args) {
+      Optional<Warrior> optWarrior = Warrior.getWarrior();
+
+      Optional<Warrior> strongestWarrior = getEmpty()
+          .or(() -> Warrior.getWarrior())
+          .or(() -> optWarrior);
+
+      System.out.println(strongestWarrior.get().name);
+  }
+
+
+  static Optional<Warrior> getEmpty() {
+    return Optional.empty();
+  }
+
+
+  static class Warrior {
+    private String name;
+
+    Warrior(String name) {
+      this.name = name;
+    }
+    static Optional<Warrior> getWarrior() {
+      return Optional.of(new Warrior("Kratos"));
+    }
+
+  }
+}
