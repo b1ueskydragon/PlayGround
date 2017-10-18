b611daddb125e25c29296abb0e84b6d95fff1180
jang <jang_inhwa@microad.co.jp>
2017-10-18 12:54:15 +0900

    171018

---
+++ src/quiz/FunctionsStreamChallenge.java
@@ -0,0 +1,23 @@
+package quiz;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class FunctionsStreamChallenge {
+
+  public static void main(String... args) {
+    Optional<Integer> number = Stream.iterate(0, n -> n + 1)
+        .filter(FunctionsStreamChallenge::isEven)
+        .filter(n -> n >= 5)
+        .map(n -> n * 2)
+        .limit(20)
+        .findAny();
+
+    System.out.println(number);
+
+  }
+
+  private static boolean isEven(int number) {
+    return number % 2 == 0;
+  }
+}
