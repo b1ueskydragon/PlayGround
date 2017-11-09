d190a3e51f45bf1eebfacd994f3a3c723b300a20
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-11-09 10:22:14 +0900

    171109

--- src/quiz/LogicalOperatorsChallenge3.java
+++ src/quiz/LogicalOperatorsChallenge3.java
@@ -1,5 +1,12 @@
 package quiz;
 
+/**
+ * i ++ 判定・処理後のインクリ(後置インクリメント)
+ * ++ i 判定・処理前のインクリ(前置インクリメント)
+ *
+ * &・| 前の判定結果の TF に関わらず後ろも判定・処理する
+ * &&・|| 前の判定結果の TF により後ろを判定・処理する場合としない場合がある
+ */
 public class LogicalOperatorsChallenge3 {
 
   public static void main(String[] args) {
@@ -15,5 +22,19 @@ public class LogicalOperatorsChallenge3 {
     }
 
     System.out.println(num1 + ":" + num2);
+
+
+    int num3 = 7;
+    int num4 = 7;
+
+    if (num3 > num4 & num3++ > ++num4) {
+      num3++;
+    }
+
+    if (++num4 > num3 | num3++ == num4++) {
+      num3++;
+    }
+
+    System.out.println(num3 + ":" + num4);
   }
 }
