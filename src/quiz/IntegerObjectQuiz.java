c21a328b945e71a11ad32dde6e306bfe9b161cc2
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-11-10 10:11:46 +0900

    171110

---
+++ src/quiz/IntegerObjectQuiz.java
@@ -0,0 +1,18 @@
+package quiz;
+
+public class IntegerObjectQuiz {
+
+  public static void main(String[] args) {
+
+    int i1 =1;
+    Integer io = new Integer(i1);
+    Integer ib1 = i1;
+    Integer ib2 = 1;
+
+    System.out.println(
+        (i1 == io) + " " +
+        (ib1 == ib2)
+    );
+    System.out.println((io == ib2));
+  }
+}
