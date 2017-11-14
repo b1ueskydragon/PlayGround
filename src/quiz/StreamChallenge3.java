02fcea92d4d3ab0c4a10acf8864cb519ab96ab4f
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-11-14 10:24:00 +0900

    171114

--- src/quiz/StreamChallenge3.java
+++ src/quiz/StreamChallenge3.java
@@ -6,6 +6,12 @@ import java.util.stream.IntStream;
 public class StreamChallenge3 {
 
   public static void main(String[] args) {
+    // 出力される例
+    OptionalInt _reduced = IntStream.range(1, 4).reduce((a, b) -> a + b);
+
+    System.out.println(_reduced.getAsInt());
+
+    // 本題
     OptionalInt reduced = IntStream.range(4, 1).reduce((a, b) -> a + b);
 
     System.out.println(reduced.getAsInt());
