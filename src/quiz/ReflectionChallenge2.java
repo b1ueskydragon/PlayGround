b3071562b592bcc67a6f83737d8c91294ba9b1d2
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-11-17 10:35:31 +0900

    171117

--- src/quiz/ReflectionChallenge2.java
+++ src/quiz/ReflectionChallenge2.java
@@ -3,10 +3,17 @@ package quiz;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * リフレクションは、文字列で指定されたクラスのメソッドの実行などを可能にし、
+ *　プログラムの自由度を向上させる便利な技法。
+ *  その分、副作用などに気をつければならない。
+ *  (クラス名変更による class not found など)
+ */
 public class ReflectionChallenge2 {
 
   public static void main(String... args) {
     System.out.println(Jedi.class.getAnnotation(Table.class).name());
+    System.out.println(Jedi.class.getAnnotation(Fire.class).fire());
     System.out.println(Jedi.class.getAnnotation(Column.class).name());
   }
 
@@ -20,6 +27,15 @@ public class ReflectionChallenge2 {
     }
   }
 
+  @Fire(fire = "fire")
+  private static class AttackType {
+    String fire;
+
+    public String getFire() {
+      return fire;
+    }
+  }
+
   @Retention(RetentionPolicy.RUNTIME)
   @interface Table {
     String name();
@@ -27,5 +43,10 @@ public class ReflectionChallenge2 {
 
   @interface Column {
     String name();
+  }
+
+  @Retention(RetentionPolicy.RUNTIME)
+  @interface Fire {
+    String fire();
   }
 }
