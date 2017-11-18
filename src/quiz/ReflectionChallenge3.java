d153f1c044a63ad68cded298ddd943c0fde00432
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-11-18 20:33:44 +0900

    171118

--- src/quiz/ReflectionChallenge3.java
+++ src/quiz/ReflectionChallenge3.java
@@ -6,8 +6,8 @@ public class ReflectionChallenge3 {
 
   public static void main(String[] args) throws Exception {
 
-    Method method = ReflectionChallenge3.class.getDeclaredMethod("doStuff");
-    method.invoke(new ReflectionChallenge3());
+    Method method = ReflectionChallenge3.class.getDeclaredMethod("doStuff"); // このクラスの全てのメソッドの読み込み
+    method.invoke(new ReflectionChallenge3()); // invoke 該当メソッドを実行する
 
     Method method2 = ReflectionChallenge3.class.getMethod("doStuff"); // doStuff() is not public
     method2.invoke(new ReflectionChallenge3());
