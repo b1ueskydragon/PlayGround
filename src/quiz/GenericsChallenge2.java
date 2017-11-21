10cde76e108d40115bef0c19f27087b461754f4c
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-11-21 10:05:44 +0900

    171121

---
+++ src/quiz/GenericsChallenge2.java
@@ -0,0 +1,20 @@
+package quiz;
+
+public class GenericsChallenge2 {
+
+  public static void main(String[] args) {
+    Archer archer = new Archer();
+
+    archer.attack("->");
+    archer.attack(Double.valueOf(1.0));
+    archer.attack(Float.valueOf(1));
+  }
+
+  static class Archer<T> {
+    T t;
+
+    void attack(T t) {
+      System.out.println(this.t);
+    }
+  }
+}
