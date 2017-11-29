ca6a620839ee21acf024f9de85facef86b8210e4
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-11-29 15:41:31 +0900

    171129+a

--- src/quiz/CastChallenge.java
+++ src/quiz/CastChallenge.java
@@ -4,14 +4,31 @@ public class CastChallenge {
 
   public static void main(String[] args) {
 
-    int number1 = (int) (float) 2.0;
-    double number2 = (Double) (Object) 2.0;
-    float number3 = (Float) 2f;
+    int number1 = (int) (float) 2.0; // 2
+    double number2 = (Double) (Object) 2.0; // 2.0
+    float number3 = (Float) 2f; // 2.0
+    char number4 = 2; // 2
+    long number5 = (Long) (long) (float) 2d; // 2
 
-    char number4 = 2;
-    long number5 = (Long) (long) (float) 2d;
+    // type char samples
+    char number4a = '2';
+    System.out.println(number4a); // 2
+    System.out.println((int) number4a); // 50
+    System.out.println((char) 50); // 2
 
-    System.out.println(number1 + number2 + number3 + number4 + number5);
+    System.out.println(number1 + number2 + number3 + number4 + number5); // 10.0
 
+    // sum of double and int ...
+    System.out.println(1.0 + 1); // 1.0
+
+    int a = 10;
+    char ch = 'h';
+    int sum = a + ch;
+    //System.out.println((int)ch);
+    System.out.println(sum);
+
+    int y = 3;
+    double z = y + 4.0;
+    System.out.println(z);
   }
 }
