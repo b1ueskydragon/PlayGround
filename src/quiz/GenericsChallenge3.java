b55763eda9392ecbce486af76901cf13faa4036c
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-12-01 09:07:03 +0900

    171201

---
+++ src/quiz/GenericsChallenge3.java
@@ -0,0 +1,29 @@
+package quiz;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class GenericsChallenge3 {
+
+  public static void main(String[] args) {
+    List<String> firstResult = GenericsChallenge3.<String>get(new ArrayList<>(), new String("1"));
+
+    List<Object> secondResult = GenericsChallenge3.get("Homer", Double.valueOf("4"));
+
+    Stream<Object> stream = Stream.concat(firstResult.stream(), secondResult.stream());
+    stream.forEach(System.out::println);
+  }
+
+  public static <T> List<T> get(List<T> list, T t) {
+    list.add(t);
+    return list;
+  }
+
+  public static <T> List<T> get(T type1, T type2) {
+    List<T> list = new ArrayList<>();
+    list.add(type1);
+    list.add(type2);
+    return list;
+  }
+}
