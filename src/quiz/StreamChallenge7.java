0ed0b4f5bb88c4daea3d150ed19c713ca020d440
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-12-07 11:35:21 +0900

    171207 intstream iterate

--- src/quiz/StreamChallenge7.java
+++ src/quiz/StreamChallenge7.java
@@ -5,6 +5,9 @@ import java.util.stream.IntStream;
 public class StreamChallenge7 {
 
   public static void main(String[] args) {
+    /**
+     * start 10, predicate, decrement by 2
+     */
     IntStream.iterate(10, i -> i >= 0, i -> i - 2) // seed(初期要素) , hasNext(関数), next(関数)
         .filter(i -> i > 5 && i < 10)
         .sorted()
