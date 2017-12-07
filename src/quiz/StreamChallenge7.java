8fabef0eac18c5078099b32a4ff7e01fc55ca8e6
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-12-07 11:30:54 +0900

    171207 intstream iterate

---
+++ src/quiz/StreamChallenge7.java
@@ -0,0 +1,13 @@
+package quiz;
+
+import java.util.stream.IntStream;
+
+public class StreamChallenge7 {
+
+  public static void main(String[] args) {
+    IntStream.iterate(10, i -> i >= 0, i -> -2) // seed(初期要素) , hasNext(関数), next(関数)
+        .filter(i -> i > 5 && i < 10)
+        .sorted()
+        .forEach(System.out::println);
+  }
+}
