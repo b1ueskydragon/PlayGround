fa640fca4a60e939e34dd664295e07da1b9d02ec
jang <jang_inhwa@microad.co.jp>
2017-12-08 12:01:51 +0900

    Practice of take and dropWhile

---
+++ src/quiz/StreamChallenge8_.java
@@ -0,0 +1,54 @@
+package quiz;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * takeWhile
+ * dropWhile
+ *
+ * 最初の要素の段階でひっかからないと、何も take || drop されない
+ */
+public class StreamChallenge8_ {
+  public static void main(String[] args) {
+    List<Integer> myList = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+    myList.stream()
+        .takeWhile(i -> i % 2 == 0)
+        .forEach(System.out::println);
+
+    // 0
+
+    System.out.println("- - - - - - - - - - -");
+
+    List<Integer> moreList = Arrays.asList(0, 2, 3, 4, 5, 6, 7, 8, 9);
+
+    moreList.stream()
+        .takeWhile(i -> i % 2 == 0)
+        .forEach(System.out::println);
+
+    // 2
+
+    System.out.println("- - - - - - - - - - -");
+
+    List<Integer> yourList = Arrays.asList(100, 90, 7, 6, 1);
+
+    yourList.stream()
+        .takeWhile(e -> e < 100)
+        .dropWhile(e -> e < 50)
+        .forEach(System.out::println);
+
+    // null
+
+    System.out.println("- - - - - - - - - - -");
+
+    List<Integer> ourList = Arrays.asList(1, 6, 7, 90, 100);
+
+    ourList.stream()
+        .takeWhile(e -> e < 100)
+        .dropWhile(e -> e < 50)
+        .forEach(System.out::println);
+
+    // 90
+  }
+}
