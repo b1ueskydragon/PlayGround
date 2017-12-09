60b178d56899bb8025e7a45bc71ba7fe3e707bd9
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-12-10 07:48:43 +0900

    171210 add memo

--- src/quiz/StreamChallenge9.java
+++ src/quiz/StreamChallenge9.java
@@ -6,12 +6,12 @@ import java.util.stream.Collectors;
 public class StreamChallenge9 {
 
   public static void main(String[] args) {
-    List<Integer> numbers = List.of(1, 3, 5, 7, 10);
+    List<Integer> numbers = List.of(1, 3, 5, 7, 10); // immutable list
 
     numbers.addAll(numbers.stream()
-        .filter(i -> i > 10)
+        .filter(i -> i > 10) // size changed
         .collect(Collectors.toList()));
 
-    numbers.forEach(System.out::println);
+    numbers.forEach(System.out::println); // ImmutableCollections -> UnsupportedOperationException
   }
 }
