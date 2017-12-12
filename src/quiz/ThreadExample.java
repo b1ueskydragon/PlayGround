67cc09697e94662f8b8e2ba570cbb74961428d4c
jang <jang_inhwa@microad.co.jp>
2017-12-12 17:29:58 +0900

    171212 join practice

--- src/quiz/ThreadExample.java
+++ src/quiz/ThreadExample.java
@@ -37,6 +37,17 @@ public class ThreadExample {
     thread4.start();
     thread4.join();
 
+    // how about this ?
+//    thread1.start();
+//    thread2.start();
+//    thread3.start();
+//    thread4.start();
+//    thread1.join();
+//    thread2.join();
+//    thread3.join();
+//    thread4.join();
+
+
     Thread thread5 = new Thread(runnableJob, "T5");
     Thread thread6 = new Thread(runnableJob, "T6");
     Thread thread7 = new Thread(runnableJob, "T7");
