1e07e782012c8705dfe64e087820b1af8ee1fb2b
jang <jang_inhwa@microad.co.jp>
2017-12-13 11:49:19 +0900

    171213 arrange

--- src/quiz/MethodReferenceChallenge2.java
+++ src/quiz/MethodReferenceChallenge2.java
@@ -5,11 +5,15 @@ public class MethodReferenceChallenge2 {
   public static void main(String[] args) {
     String shoot = shoot(Soprano::useRevolver);
 
+    // 匿名クラスで実装と同時にインスタンス化ー
     System.out.println(shoot + new Gun() {
       public String shoot() {
         return null;
       }
     }.reload());
+
+    // same as above
+    System.out.println(shoot + new GunImpl().reload());
   }
 
   static String shoot(Gun gun) {
@@ -27,6 +31,14 @@ public class MethodReferenceChallenge2 {
   static class Soprano {
     static String useRevolver() {
       return "shot";
+    }
+  }
+
+  // 匿名クラスを使わない場合
+  static class GunImpl implements Gun {
+    @Override
+    public String shoot() {
+      return null;
     }
   }
 }
