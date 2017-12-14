35c192a8a67e6361ced4df9aa0f4cb063a2c485f
jang <jang_inhwa@microad.co.jp>
2017-12-14 13:46:55 +0900

    what is it

---
+++ src/code/with/play/Quanc.java
@@ -0,0 +1,27 @@
+package code.with.play;
+
+import java.util.Arrays;
+
+/**
+ * 꿩
+ */
+public class Quanc {
+
+  private final static char mars = '\u25A3';
+  private final static char white = '\u25A1';
+  private final static char[][] background = new char[15][15];
+
+  public static void main(String[] args) {
+
+    for (int i = 0; i < 15; i++) {
+      for (int j = 0; j < 15; j++) {
+        if (j % 2 == 0) background[i][j] = white;
+        else background[i][j] = mars;
+      }
+    }
+
+    for (int i = 0; i < 15; i++) {
+        System.out.println(background[i]);
+    }
+  }
+}
