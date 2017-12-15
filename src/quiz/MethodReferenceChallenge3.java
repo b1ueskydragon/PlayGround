ef639b1c4b282a3d31b57e765a742d53e4aa9022
jang <jang_inhwa@microad.co.jp>
2017-12-15 12:05:36 +0900

    171215 add practice

--- src/quiz/MethodReferenceChallenge3.java
+++ src/quiz/MethodReferenceChallenge3.java
@@ -1,10 +1,34 @@
 package quiz;
 
+import java.lang.reflect.Method;
+import java.util.function.Consumer;
+
+/**
+ * メソッド参照
+ *
+ * 関数型インターフェースの変数にメソッドそのものを代入すること.
+ *
+ * static メソッド
+ * クラス名::メソッド名
+ *
+ * インスタンスメソッド
+ * インスタンス変数名::メソッド名
+ *
+ *
+ * 関数型インターフェース
+ *
+ * 抽象メソッドが1つだけ定義されているインターフェース.
+ * ラムダ式やメソッド参照の代入先になれるインターフェースのこと.
+ *
+ */
 public class MethodReferenceChallenge3 {
 
   public static void main(String[] args) {
     attack(MethodReferenceChallenge3::printAttack);
     jump(MethodReferenceChallenge3::printJump);
+
+    MethodReferenceChallenge3 mrc = new MethodReferenceChallenge3();
+    mrc.coding(mrc::printCoding);
   }
 
   interface Luke {
@@ -31,5 +55,18 @@ public class MethodReferenceChallenge3 {
 
   static void attack(Luke luke) {
     luke.attack();
+  }
+
+
+  interface Inagon {
+    void coding();
+  }
+  // 非 static インスタンスメソッド
+  void coding(Inagon inagon) {
+    inagon.coding();
+  }
+
+  void printCoding(){
+    System.out.println("coding");
   }
 }
