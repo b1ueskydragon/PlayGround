e0e86696414c4f96b61bbf2986093b96a5b747b8
jang <jang_inhwa@microad.co.jp>
2017-12-15 11:37:44 +0900

    171215

---
+++ src/quiz/MethodReferenceChallenge3.java
@@ -0,0 +1,35 @@
+package quiz;
+
+public class MethodReferenceChallenge3 {
+
+  public static void main(String[] args) {
+    attack(MethodReferenceChallenge3::printAttack);
+    jump(MethodReferenceChallenge3::printJump);
+  }
+
+  interface Luke {
+    void attack();
+  }
+
+  interface Anakin {
+    void jump();
+  }
+
+  static void jump(Anakin anakin) {
+    anakin.jump();
+  }
+
+  static void printAttack() {
+    System.out.println("attack");
+  }
+
+  static String printJump() {
+    String jump = "jump";
+    System.out.println(jump);
+    return jump;
+  }
+
+  static void attack(Luke luke) {
+    luke.attack();
+  }
+}
