bdaa19b773a00de2368351241e3518d5ca30d131
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-12-18 11:04:16 +0900

    171218

---
+++ src/quiz/GenericsChallenge5.java
@@ -0,0 +1,31 @@
+package quiz;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsChallenge5 {
+
+  public static void main(String[] args) {
+    List<Integer> finalList;
+
+    List<Integer> integerList = new ArrayList<>();
+    integerList.add(1);
+    finalList = addInto(integerList);
+
+    List<Long> longList = new ArrayList<>();
+    finalList.addAll(addInto(longList));
+
+    List<BigDecimal> bigDecimalList = new ArrayList<>();
+    bigDecimalList.add(BigDecimal.ZERO);
+    finalList.addAll(addInto(bigDecimalList));
+
+    finalList.forEach(System.out::println);
+  }
+
+  static List addInto(List<? extends Number> list) {
+    List<Number> resultList = new ArrayList<>();
+    resultList.addAll(list);
+    return resultList;
+  }
+}
