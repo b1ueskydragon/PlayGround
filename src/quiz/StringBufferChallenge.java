919807f489e8963937dcc47d639af03c3b5e9a3b
jang <jang_inhwa@microad.co.jp>
2017-12-27 15:24:34 +0900

    171227

---
+++ src/quiz/StringBufferChallenge.java
@@ -0,0 +1,13 @@
+package quiz;
+
+public class StringBufferChallenge {
+  public static void main(String... args) {
+    Object nullObj = null;
+    StringBuffer buffer = new StringBuffer(10);
+    buffer.append("nobugs ");
+    buffer.append("project ");
+    buffer.append(nullObj);
+    buffer.insert(14, '1'); // int offset, char c
+    System.out.println(buffer);
+  }
+}
