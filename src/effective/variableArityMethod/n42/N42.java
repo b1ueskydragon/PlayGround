61331f746d71596873092ca409347af170ca9a4e
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2018-01-18 17:42:17 +0900

    180118 args

--- src/effective/variableArityMethod/n42/N42.java
+++ src/effective/variableArityMethod/n42/N42.java
@@ -9,8 +9,14 @@ public class N42 {
   }
 
   public static void main(String[] args) {
-    System.out.println(sum(1, 2, 3));
-    System.out.println(sum());
+//    System.out.println(sum(1, 2, 3));
+//    System.out.println(sum());
+
+    int sum = 0;
+    for (int i = 0; i < args.length; i++)
+      sum += Integer.parseInt(args[i]);
+
+    System.out.println(sum);
   }
 }
 
