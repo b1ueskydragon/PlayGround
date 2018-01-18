b57809eda9e10b3c8d2bc42ee5f70f13260eb15f
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2018-01-18 18:29:18 +0900

    180118 more simply

--- src/effective/variableArityMethod/n42/N42.java
+++ src/effective/variableArityMethod/n42/N42.java
@@ -1,6 +1,7 @@
 package effective.variableArityMethod.n42;
 
 public class N42 {
+  // 可変長引数の単純な利用(0個以上の引数)
   static int sum(int... args) {
     int sum = 0;
     for (int arg : args)
@@ -8,15 +9,28 @@ public class N42 {
     return sum;
   }
 
+  // 1個以上の引数を渡すための正しい可変長引数の利用方法
+  static int min(int firstArg, int... remainingArgs) {
+    int min = firstArg;
+    for (int arg : remainingArgs)
+      if (arg < min)
+        min = arg;
+    return min;
+  }
+
   public static void main(String[] args) {
-//    System.out.println(sum(1, 2, 3));
-//    System.out.println(sum());
+    System.out.println(sum(1, 2, 3));
+    System.out.println(sum());
+
+    System.out.println(min(10, -1, 2, 3));
 
     int sum = 0;
-    for (int i = 0; i < args.length; i++)
-      sum += Integer.parseInt(args[i]);
+
+    for (String arg : args)
+      sum += Integer.parseInt(arg);
 
     System.out.println(sum);
   }
+
 }
 
