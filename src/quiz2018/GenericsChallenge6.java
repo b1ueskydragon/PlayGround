ef48f84b2c7897ba0167676569ef22c3e3eb31c9
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2018-01-20 09:40:37 +0900

    180120

---
+++ src/quiz2018/GenericsChallenge6.java
@@ -0,0 +1,22 @@
+package quiz2018;
+
+public class GenericsChallenge6 {
+
+  public static void main(String... args) {
+    new Homer<>("goHomer", 10) {}.<Double>print(10D);
+  }
+
+  static class Homer<T, O extends Integer> {
+    T t;
+    O o;
+
+    public Homer(T t, O o) {
+      this.t = t;
+      this.o = o;
+    }
+
+    <Krusty extends Number> void print(Krusty krusty) {
+      System.out.println(t + "" + (o.intValue() + 2) + krusty.intValue());
+    }
+  }
+}
