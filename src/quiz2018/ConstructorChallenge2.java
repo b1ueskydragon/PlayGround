6c66e0649c31c3d1d654b542f1e1247c5f392370
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2018-01-25 09:30:51 +0900

    180125 to encapsulate

--- src/quiz2018/ConstructorChallenge2.java
+++ src/quiz2018/ConstructorChallenge2.java
@@ -6,10 +6,10 @@ public class ConstructorChallenge2 {
     Superpower.print();
   }
 
-  static class Superpower {
+  private static class Superpower {
     private static String superpower = "";
 
-    public Superpower(String weapon, String action, int force) {
+    Superpower(String weapon, String action, int force) {
       superpower += weapon + "," + action + "," + force;
     }
 
@@ -18,16 +18,16 @@ public class ConstructorChallenge2 {
     }
   }
 
-  static class Neo extends Superpower {
-    public Neo(String action) {
+  private static class Neo extends Superpower {
+    Neo(String action) {
       this(action, 99);
     }
 
-    public Neo(String action, int force) {
+    Neo(String action, int force) {
       this("revolver", action, force);
     }
 
-    public Neo(String weapon, String action, int force) {
+    Neo(String weapon, String action, int force) {
       super(weapon, action, force);
     }
   }
