5014f9913bb238d7554e1091e528a17217d9e459
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2018-01-28 01:36:39 +0900

    h27 Need to know about char int

---
+++ src/graffiti/HelloChar.java
@@ -0,0 +1,11 @@
+package graffiti;
+
+public class HelloChar {
+  public static void main(String... args) {
+    char c = 'a';
+    System.out.println(c);
+    System.out.println((int) c);
+    System.out.println(c == (int) c);
+    System.out.println(c < 256);
+  }
+}
