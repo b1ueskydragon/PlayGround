2206d1cca1f3184c3b6beb09131d2c3908961af1
jang <jang_inhwa@microad.co.jp>
2018-01-31 17:11:18 +0900

    Scala 99 P08 arrange : Rename var

--- src/graffiti/Compressing.java
+++ src/graffiti/Compressing.java
@@ -6,29 +6,28 @@ import java.util.stream.Collectors;
 
 public class Compressing {
   public static void main(String[] args) {
-    List<Object> list = Arrays.asList("a", 1, "b", 1, "b", "c", "c", "d", "a");
+    List<Object> list = Arrays.asList("a", 1, "b", 1, "b", "c", "c", "d", "a", 1);
 
     for (int i = 0; i < list.size() - 1; i++) {
-      list = generalCompress(list, list.get(i), i);
       if (i == list.size() - 1) break;
+      list = generalCompress(list, i);
     }
+
     list.forEach(System.out::println);
   }
 
-  private static <T> List<T> generalCompress(List<T> list, T head, int newHeadInx) {
+
+  private static <T> List<T> generalCompress(List<T> list, int comparisonIdx) {
+
+    T comparisonHead = list.get(comparisonIdx); // 基準となる List の head (2回目からは List の tail の head を想定) を動的に設定.
+
     List<T> result = list
         .stream()
-        .filter(el -> !el.equals(head))
+        .filter(el -> !el.equals(comparisonHead))
         .collect(Collectors.toList());
 
-    result.add(newHeadInx, head);
-    return result;
-  }
+    result.add(comparisonIdx, comparisonHead);
 
-  // prototype
-  private static List<Object> compress(List<Object> list, Object head, int newHeadInx) {
-    List<Object> result = list.stream().filter(el -> !el.equals(head)).collect(Collectors.toList());
-    result.add(newHeadInx, head);
     return result;
   }
 }
