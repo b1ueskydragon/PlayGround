a683bade97ed63f4563622010347b86b725f7217
jang <jang_inhwa@microad.co.jp>
2018-01-31 17:03:46 +0900

    Scala 99 P08 : remove try-catch

--- src/graffiti/Compressing.java
+++ src/graffiti/Compressing.java
@@ -6,25 +6,26 @@ import java.util.stream.Collectors;
 
 public class Compressing {
   public static void main(String[] args) {
-    List<Object> strDuplicatedList = Arrays.asList("a", 1, "b", 1, "b", "c", "c", "d", "a");
+    List<Object> list = Arrays.asList("a", 1, "b", 1, "b", "c", "c", "d", "a");
 
-    for (int i = 0; i < strDuplicatedList.size() - 1; i++) {
-      try {
-        strDuplicatedList = generalCompress(strDuplicatedList, strDuplicatedList.get(i), i);
-      } catch (IndexOutOfBoundsException e) { // TODO 例外がなくても動くプログラムに変更
-        break;
-      }
+    for (int i = 0; i < list.size() - 1; i++) {
+      list = generalCompress(list, list.get(i), i);
+      if (i == list.size() - 1) break;
     }
-    strDuplicatedList.forEach(System.out::println);
-
+    list.forEach(System.out::println);
   }
 
   private static <T> List<T> generalCompress(List<T> list, T head, int newHeadInx) {
-    List<T> result = list.stream().filter(el -> !el.equals(head)).collect(Collectors.toList());
+    List<T> result = list
+        .stream()
+        .filter(el -> !el.equals(head))
+        .collect(Collectors.toList());
+
     result.add(newHeadInx, head);
     return result;
   }
 
+  // prototype
   private static List<Object> compress(List<Object> list, Object head, int newHeadInx) {
     List<Object> result = list.stream().filter(el -> !el.equals(head)).collect(Collectors.toList());
     result.add(newHeadInx, head);
