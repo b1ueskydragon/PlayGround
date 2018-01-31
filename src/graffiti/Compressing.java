a51e054d55084091141044dc1ee974e8c517bb71
jang <jang_inhwa@microad.co.jp>
2018-01-31 17:15:32 +0900

    Scala 99 P08 arrange : Add javadoc

--- src/graffiti/Compressing.java
+++ src/graffiti/Compressing.java
@@ -4,6 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Arrange of  http://aperiodic.net/phil/scala/s-99/ 's P08.
+ *
+ * 重複する要素を全て除去して, かつ, もとの順序を守って圧縮したリストを出力する.
+ */
 public class Compressing {
   public static void main(String[] args) {
     List<Object> list = Arrays.asList("a", 1, "b", 1, "b", "c", "c", "d", "a", 1);
@@ -16,7 +21,15 @@ public class Compressing {
     list.forEach(System.out::println);
   }
 
-
+  /**
+   * 汎用的な型のリストを受付, 圧縮を行う.
+   *
+   * @param list          リスト
+   * @param comparisonIdx 比較基準となる要素のインデックス
+   * @param <T>           この段階では決まっていない要素の型
+   * @return              圧縮を一度施したリスト
+   */
+  // TODO ループもこの中で完結させる.
   private static <T> List<T> generalCompress(List<T> list, int comparisonIdx) {
 
     T comparisonHead = list.get(comparisonIdx); // 基準となる List の head (2回目からは List の tail の head を想定) を動的に設定.
