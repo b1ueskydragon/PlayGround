b9695820b26954fb0d33000697747c1e8e8b8f6b
jang <jang_inhwa@microad.co.jp>
2018-02-08 14:01:21 +0900

    Trash

--- src/dailyCodingProblem/P01.java
+++ src/dailyCodingProblem/P01.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Queue;
 
 /**
  * This problem was asked by Google.
@@ -11,56 +12,63 @@ import java.util.NoSuchElementException;
  * This should be done in-place.
  * Recall that you can only push or pop from a stack, and enqueue or dequeue from a queue.
  * For example, if the stack is [1, 2, 3, 4, 5], it should become [1, 5, 2, 4, 3]. If the stack is [1, 2, 3, 4], it should become [1, 4, 2, 3].
- * Hint: Try working backwards from the end state.
+ * <p>
+ * Hint: Try working backwards (訳: 逆向き解決法 ?) from the end state.
  */
 public class P01 {
+  private static final List<Integer> TESTLIST = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+  private static final int SIZE = TESTLIST.size();
 
-  // TODO Make method.
-  private static final List<Integer> testList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+  private static List<Integer> interleave(List<Integer> list) {
+    if (list.isEmpty()) throw new NoSuchElementException();
 
-  /*
-   * Stack
-   */
-  private static void push(List<Integer> stack, int el) {
-    if (stack.isEmpty()) return;
+    List<Integer> firstHalf = new ArrayList<>();
+    List<Integer> secondHalf = new ArrayList<>();
 
-    stack.add(el);
+    if (SIZE % 2 == 0) {
+      int centerIdx = list.size() / 2 - 1;
+
+      // First half: indexes that consumed by pop.
+      for (int i = 0; i <= centerIdx; i++) {
+        firstHalf.add(list.get(i));
+      }
+
+      // Second half: indexes that consumed by dequeue.
+      for (int i = list.size() - 1; i >= centerIdx; i--) {
+        secondHalf.add(list.get(i));
+      }
+    } else {
+      int centerIdx = list.size() / 2;
+
+      // First half: indexes that consumed by pop.
+      for (int i = 0; i < centerIdx; i++) {
+        firstHalf.add(list.get(i));
+      }
+
+      // Second half: indexes that consumed by dequeue.
+      for (int i = list.size() - 1; i > centerIdx; i--) {
+        secondHalf.add(list.get(i));
+      }
+    }
+
+
+
+    return merge(firstHalf, secondHalf);
   }
 
-  private static int pop(List<Integer> stack) {
-    if (stack.isEmpty()) throw new NoSuchElementException();
+  private static List<Integer> merge(List<Integer> firstHalf, List<Integer> secondHalf) {
+    List<Integer> result = new ArrayList<>();
 
-    stack.remove(stack.size() - 1);
+    result.add(firstHalf.get(0));
+    result.add(secondHalf.get(0));
+    result.add(firstHalf.get(1));
+    result.add(secondHalf.get(1));
 
-    return stack.get(stack.size() - 1);
-  }
-
-  /*
-   * Queue
-   */
-  private static void enqueue(List<Integer> queue, int el) {
-    if (queue.isEmpty()) return;
-
-    queue.add(0, el);
-  }
-
-  private static int dequeue(List<Integer> queue) {
-    if (queue.isEmpty()) throw new NoSuchElementException();
-
-    Integer result = queue.get(0);
-
-    queue.remove(0);
 
     return result;
   }
 
-
-  private static int[] interleave(int[] ary) {
-
-    return ary;
-  }
-
   public static void main(String... args) {
-
+    interleave(TESTLIST).forEach(System.out::println);
   }
 }
