21af2b21c23abd292304d86ab01f367009f1c4ac
jang <jang_inhwa@microad.co.jp>
2018-02-08 14:26:45 +0900

    Using Stack and Queue

--- src/dailyCodingProblem/P01.java
+++ src/dailyCodingProblem/P01.java
@@ -1,10 +1,9 @@
 package dailyCodingProblem;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.NoSuchElementException;
+
+import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 /**
  * This problem was asked by Google.
@@ -16,59 +15,31 @@ import java.util.Queue;
  * Hint: Try working backwards (訳: 逆向き解決法 ?) from the end state.
  */
 public class P01 {
-  private static final List<Integer> TESTLIST = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
-  private static final int SIZE = TESTLIST.size();
 
-  private static List<Integer> interleave(List<Integer> list) {
-    if (list.isEmpty()) throw new NoSuchElementException();
-
-    List<Integer> firstHalf = new ArrayList<>();
-    List<Integer> secondHalf = new ArrayList<>();
-
-    if (SIZE % 2 == 0) {
-      int centerIdx = list.size() / 2 - 1;
-
-      // First half: indexes that consumed by pop.
-      for (int i = 0; i <= centerIdx; i++) {
-        firstHalf.add(list.get(i));
-      }
-
-      // Second half: indexes that consumed by dequeue.
-      for (int i = list.size() - 1; i >= centerIdx; i--) {
-        secondHalf.add(list.get(i));
-      }
-    } else {
-      int centerIdx = list.size() / 2;
-
-      // First half: indexes that consumed by pop.
-      for (int i = 0; i < centerIdx; i++) {
-        firstHalf.add(list.get(i));
-      }
-
-      // Second half: indexes that consumed by dequeue.
-      for (int i = list.size() - 1; i > centerIdx; i--) {
-        secondHalf.add(list.get(i));
-      }
-    }
-
-
-
-    return merge(firstHalf, secondHalf);
+  private static Stack<Integer> createStack(int... ii) {
+    Stack<Integer> stack = new Stack<>();
+    for (int i : ii) stack.push(i);
+    return stack;
   }
 
-  private static List<Integer> merge(List<Integer> firstHalf, List<Integer> secondHalf) {
-    List<Integer> result = new ArrayList<>();
+  private static void interleave(Stack<Integer> stack, Queue<Integer> queue) {
 
-    result.add(firstHalf.get(0));
-    result.add(secondHalf.get(0));
-    result.add(firstHalf.get(1));
-    result.add(secondHalf.get(1));
+    for (int i = 0; i < stack.size() / 2; i++) {
+      queue.add(stack.pop());
+    }
 
+    System.out.println("que");
+    queue.forEach(System.out::println);
 
-    return result;
   }
 
   public static void main(String... args) {
-    interleave(TESTLIST).forEach(System.out::println);
+
+    Stack<Integer> stack = createStack(1, 2, 3, 4);
+
+    interleave(stack, new LinkedList<>());
+
+    System.out.println("sta");
+    stack.forEach(System.out::println);
   }
 }
