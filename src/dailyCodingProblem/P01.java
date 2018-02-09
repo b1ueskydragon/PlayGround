ece828c5ce41d54782277c22e0756170be459f1d
inagon <dragoalie@gmail.com>
2018-02-09 09:24:30 +0900

    Parameter T

--- src/dailyCodingProblem/P01.java
+++ src/dailyCodingProblem/P01.java
@@ -7,7 +7,7 @@ import java.util.Stack;
 
 /**
  * @challenger b1ueskydragon
- *
+ * <p>
  * This problem was asked by Google.
  * Given a stack of N elements, interleave the first half of the stack with the second half reversed using only one other queue.
  * This should be done in-place.
@@ -17,13 +17,11 @@ import java.util.Stack;
  * Hint: Try working backwards (訳: 逆向き解決法 ?) from the end state.
  */
 public class P01 {
-
-  // TODO : Use WildCard or Type Parameter
   // TODO : Add Test
 
-  private static Stack<Integer> createStack(int... ii) {
-    Stack<Integer> stack = new Stack<>();
-    for (int i : ii) stack.push(i);
+  private static <T> Stack<T> createStack(T... args) {
+    Stack<T> stack = new Stack<>();
+    for (T arg : args) stack.push(arg);
     return stack;
   }
 
@@ -32,16 +30,16 @@ public class P01 {
     System.out.print(System.getProperty("line.separator"));
   }
 
-  private static Stack<Integer> interleave(Stack<Integer> stack) {
+  private static <T> Stack<T> interleave(Stack<T> stack) {
     if (stack.isEmpty()) throw new NoSuchElementException();
     if (stack.size() == 1) return stack;
 
     int oriSize = stack.size();
 
     // only one other queue.
-    Queue<Integer> queue = new LinkedList<>();
+    Queue<T> queue = new LinkedList<>();
     // another Stack
-    Stack<Integer> buffStack = new Stack<>();
+    Stack<T> buffStack = new Stack<>();
 
     // Using 'oriSize' helps to prevent side-effect ('stack' size is continuously modified in this loop).
     for (int i = 0; i < oriSize / 2; i++) {
