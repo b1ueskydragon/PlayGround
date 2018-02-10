2c9c0ab15c96181e47ce45762f2b6f1bf87a4556
inagon <dragoalie@gmail.com>
2018-02-11 01:05:29 +0900

    Little bit weird..

--- src/dailyOne/P02/P02.java
+++ src/dailyOne/P02/P02.java
@@ -12,15 +12,21 @@ public class P02 {
   // return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
   // Solve it without using division and in O(n) time.
   private static int[] calc(int... ary) {
-    int[] result = new int[ary.length];
-    IntStream.range(0, ary.length).forEach(i -> result[i] = 1); // make all of elements of result 1
+    int len = ary.length;
+    int[] result = new int[len];
+    IntStream.range(0, len).forEach(i -> result[i] = 1); // make all of elements of result 1
 
-    int buff = 1;
-    for (int i = 0; i < ary.length ; i++) {
-      buff *= ary[i];
-      result[i] *= buff;
+    int p = 1; // previous (ex. result[0] is 1 before for-loop)
+    int n = 1; // next (ex. result[4] is 1 before for-loop)
+    // ex.
+    // r[1] = r[1] x 1 x a[0]
+    // r[3] = r[3] x a[4] x 1
+    for (int i = 0; i < len - 1; i++) {
+      p *= ary[i];
+      result[i + 1] *= p;
+      n *= ary[len - (i + 1)];
+      result[len - (i + 2)] *= n;
     }
-
     return result;
   }
 
@@ -28,5 +34,4 @@ public class P02 {
     printAry(calc(1, 2, 3, 4, 5));
   }
   // かけ算を reduce 処理でしても O(n^2) ?
-
 }
