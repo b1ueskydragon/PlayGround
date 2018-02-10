f5ccbe0ac89d9cc62c88c15c2dab36faa27f7cc3
inagon <dragoalie@gmail.com>
2018-02-10 11:55:33 +0900

    Replace to using stream

--- src/dailyOne/P02/P02.java
+++ src/dailyOne/P02/P02.java
@@ -1,19 +1,16 @@
 package dailyOne.P02;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 /**
  * @challenger b1ueskydragon
- * Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
- * Solve it without using division and in O(n) time.
- * For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
  */
 public class P02 {
 
   private static int[] createAry(int... args) {
     int[] ary = new int[args.length];
-    int idx = 0;
-    for(int arg : args) ary[idx++] = arg;
+    IntStream.range(0, args.length).forEach(i -> ary[i] = args[i]);
     return ary;
   }
 
