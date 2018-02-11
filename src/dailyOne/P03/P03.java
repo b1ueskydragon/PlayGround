b120715d33962048725edbb065805de8f97d688b
inagon <dragoalie@gmail.com>
2018-02-11 13:12:58 +0900

    wip

---
+++ src/dailyOne/P03/P03.java
@@ -0,0 +1,13 @@
+package dailyOne.P03;
+
+import java.io.Serializable;
+
+/**
+ * @challenger b1ueskydragon
+ */
+public class P03 implements Serializable {
+
+  public static void main(String... args) {
+
+  }
+}
