109aa74b804daa3d5a902a3137caf2f64c1a0fb4
inagon <dragoalie@gmail.com>
2018-02-13 02:18:37 +0900

    wip

--- src/dailyOne/P04/FindLowPosInt.java
+++ src/dailyOne/P04/FindLowPosInt.java
@@ -12,7 +12,7 @@ public class FindLowPosInt {
 
   private static Integer point = 1; // Tentatively setting: outside scope (is it need?)
 
-  // TODO Arrays.contains -> return indexOf contains for-loop of array. maximum: O(N^N)
+  // Arrays.contains -> return indexOf contains for-loop of array. maximum: O(N^N)
   private static Integer findByContains(Integer... args) {
     if (args.length == 0) throw new NullPointerException();
     if (!Arrays.asList(args).contains(1)) return 1;
@@ -33,7 +33,7 @@ public class FindLowPosInt {
   private static Integer findBySort(Integer... args) {
     if (args.length == 0) throw new NullPointerException();
 
-    quickSort(args);
+    Collections.sort(Arrays.asList(args)); // quick sort
 
     int len = args.length;
     int last = len - 1;
@@ -56,47 +56,45 @@ public class FindLowPosInt {
     return put;
   }
 
-  // TODO make sort
-  private static void quickSort(Integer[] ary) {
-    Collections.sort(Arrays.asList(ary));
-  }
+  // TODO O(N)
+//  private static Integer find(Integer... args) {
+//    if (args.length == 0) throw new NullPointerException();
+//    int divisionIdx = (int) Math.ceil((args.length - 1) / 2.0);
+//
+//    int leftFind = 0;
+//    int rightFind = 0;
+//
+//
+//    for (int i = 0; i < divisionIdx; i++) {
+//      if (args[i] < 0) {
+//        args[i] = 0;
+//      }
+//
+//      if (args[0] > args[1]) {
+//        leftFind = args[1] + 1;
+//      } else {
+//        leftFind = args[0] + 1;
+//      }
+//    }
+//
+//    for (int i = divisionIdx; i< args.length -1 ; i ++) {
+//      if (args[i] < 0) {
+//        args[i] = 0;
+//      }
+//
+//      if (args[2] > args[3]) {
+//        rightFind = args[3] + 1;
+//      } else {
+//        rightFind = args[2] + 1;
+//      }
+//    }
+//
+//    int result = (leftFind < rightFind)? leftFind : rightFind;
+//
+//    return result;
+//  }
 
-
-  // O(N)
-  private static Integer find(Integer... args) {
-    if (args.length == 0) throw new NullPointerException();
-
-    int len = args.length;
-    int last = len - 1;
-
-    Integer put = args[last] + 1;
-    Integer maxValue;
-
-    for (int i = 0; i < len; i++) {
-      if (args[i] < 1) {
-        args[i] = 1;
-      } else {
-        Integer next = (i == last) ? args[last] : args[i + 1];
-
-        maxValue = (args[next] >= args[i]) ? args[next] : args[i];
-
-        if (i != 0 && args[i - 1] == 0 && args[i] != 1) {
-          return 1;
-        }
-        if (next - args[i] > 1) {
-          put = args[i] + 1;
-        }
-      }
-    }
-    return put;
-  }
 
   public static void main(String... args) {
-    System.out.println(find(3, 4, -1, 1));
-    System.out.println(find(2, 2, 2, 1, 0));
-    System.out.println(find(2, 2, 2, 1, 0));
-    System.out.println(find(1, 2, 3, 5, 6));
-    System.out.println(find(-3, -4, -2, 1, 2, 5));
-    System.out.println(find(1, 19, 1, 1, 1, 10));
   }
 }
