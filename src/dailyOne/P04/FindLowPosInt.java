a6ec3e2a99dd228aa8b6f659a0b8554ad1706597
jang <jang_inhwa@microad.co.jp>
2018-02-13 18:58:30 +0900

    WIP: Refactor O(N logN)

--- src/dailyOne/P04/FindLowPosInt.java
+++ src/dailyOne/P04/FindLowPosInt.java
@@ -30,32 +30,49 @@ public class FindLowPosInt {
     return point;
   }
 
+
+
   // O(N logN): since sort first only one time and look, so O(N logN + N)
   private static Integer findBySort(Integer... args) {
     if (args.length == 0) throw new NullPointerException();
-
-    Collections.sort(Arrays.asList(args)); // quick sort
-
+    quickSort(args, 0, args.length -1);
     int len = args.length;
-    int last = len - 1;
-    Integer put = args[last] + 1;
 
-    // TODO Simplify
-    for (int i = 0; i < len; i++) {
-      if (args[i] < 1) {
-        args[i] = 1;
+    int buff = len + 1;
+
+    for (int i = 1; i < len + 1; i++) {
+      if (args[i - 1] < 1) {
+        args[i - 1] = 1;
       }
-      else {
-        Integer next = (i == last) ? args[last] : args[i + 1];
-        if (i != 0 && args[i - 1] == 0 && args[i] != 1) {
-          return 1;
-        }
-        if (next - args[i] > 1) {
-          put = args[i] + 1;
-        }
+
+      if (args[i - 1] != i) {
+        buff = i;
       }
     }
-    return put;
+    return buff;
+  }
+
+  private static Integer[] quickSort(Integer[]ary, int left, int right) {
+    int cl = left;
+    int cr = right;
+    int centerValue = ary[(cl + cr) /2];
+
+    while (cl <= cr) {
+      while (ary[cl] < centerValue) cl ++;
+      while (centerValue < ary[cr]) cr --;
+
+      if (cl <= cr) {
+        int tmp = ary[cl];
+        ary[cl] = ary[cr];
+        ary[cr] = tmp;
+        cl++;
+        cr--;
+      }
+    }
+
+    if (left < cr) quickSort(ary, left, cr);
+    if (cl < right) quickSort(ary, cl, right);
+    return ary;
   }
 
   // O(N): Unlike List (List have to search the entire list),
@@ -70,12 +87,14 @@ public class FindLowPosInt {
   }
 
   public static void main(String... args) {
-//    System.out.println(findBySet(3, -1, 4, 1));
-//    System.out.println(findBySet(2, 0, 1));
-//    System.out.println(findBySet(2, 6, 4, 5, 1, 3));
-
+    System.out.println(findBySet(3, -1, 4, 1));
+    System.out.println(findBySet(2, 0, 1));
+    System.out.println(findBySet(2, 6, 4, 5, 1, 3));
+    System.out.println(findBySet(-100, 100, 100, 100, 99, -99, 0, 1, 2, 4));
+    System.out.println();
     System.out.println(findBySort(3, -1, 4, 1));
-//    System.out.println(findBySort(2, 0, 1));
-
+    System.out.println(findBySort(2, 0, 1));
+    System.out.println(findBySort(2, 6, 4, 5, 1, 3));
+    System.out.println(findBySet(-100, 100, 100, 100, 99, -99, 0, 1, 2, 4));
   }
 }
