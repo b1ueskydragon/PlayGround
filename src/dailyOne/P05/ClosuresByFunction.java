6d974e0cead7fdfb266288d0cdbcf656c9e6c9ba
jang <jang_inhwa@microad.co.jp>
2018-02-14 12:34:50 +0900

    Make a original pair class

--- src/dailyOne/P05/ClosuresByFunction.java
+++ src/dailyOne/P05/ClosuresByFunction.java
@@ -1,16 +1,32 @@
 package dailyOne.P05;
 
-import javafx.util.Pair;
-
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class ClosuresByFunction {
 
+  private static class Pair<T, U> {
+    private T t;
+    private U u;
+
+    Pair(T t, U u) {
+      this.t = t;
+      this.u = u;
+    }
+
+    public T getT() {
+      return t;
+    }
+
+    public U getU() {
+      return u;
+    }
+  }
+
   private static <T, U> void exeClosures(T k, U v) {
     BiFunction<T,  U, Pair<T, U>> cons = (a, b) -> new Pair<>(a, b); // Pair::new
-    Function<Pair<T, U>, T> car = pair -> pair.getKey(); // Pair::getKey
-    Function<Pair<T, U>, U> cdr = pair -> pair.getValue(); // Pair::getValue
+    Function<Pair<T, U>, T> car = pair -> pair.getT(); // Pair::getKey
+    Function<Pair<T, U>, U> cdr = pair -> pair.getU(); // Pair::getValue
 
     Pair<T, U> pair = cons.apply(k, v);
     System.out.println(car.apply(pair));
@@ -18,6 +34,6 @@ public class ClosuresByFunction {
   }
 
   public static void main(String... args) {
-    exeClosures(3, 4);
+    exeClosures(3, "4");
   }
 }
