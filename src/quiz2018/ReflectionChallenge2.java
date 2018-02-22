ab5e396e96d0a7abab45fefe8583d2b478711eb6
jang <jang_inhwa@microad.co.jp>
2018-02-22 17:15:02 +0900

    WIP: play with reflection

--- src/quiz2018/ReflectionChallenge2.java
+++ src/quiz2018/ReflectionChallenge2.java
@@ -2,18 +2,24 @@ package quiz2018;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.util.Arrays;
 
+@SuppressWarnings(value = "all")
 public class ReflectionChallenge2 {
 
-  public static void main(String... args) {
+  public static void main(String... args) throws NoSuchFieldException {
     System.out.println(Jedi.class.getAnnotation(Table.class).name());
-    System.out.println(Jedi.class.getAnnotation(Column.class).name());
+//    System.out.println(Jedi.class.getField("attackType").getAnnotation(Column.class).name());
+
+    Arrays.stream(Jedi.class.getFields()).forEach(System.out::println); // attackType should be public if we want reference this one
+
+//    System.out.println(Jedi.class.getAnnotation(Column.class).name());
   }
 
   @Table(name = "jedi")
-  static class Jedi {
+  public static class Jedi {
     @Column(name = "attack_type")
-    String attackType;
+    public String attackType;
 
     public String getAttackType() {
       return attackType;
