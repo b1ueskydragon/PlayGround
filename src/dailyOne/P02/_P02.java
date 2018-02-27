a2678538c9a9c49f61e1e9a5b356740454809452
jang <jang_inhwa@microad.co.jp>
2018-02-27 19:51:53 +0900

    Simplfy

--- src/dailyOne/P02/_P02.java
+++ src/dailyOne/P02/_P02.java
@@ -7,30 +7,24 @@ import static dailyOne.CommonUtils.*;
 public class _P02 {
 
   private static int[] calc(int... ary) {
-
     int len = ary.length;
     int[] left = new int[len];
-    IntStream.range(0, len).forEach(i -> left[i] = 1);
-
-    for (int i = 1; i < len; i++) {
-      left[i] = left[i - 1] * ary[i - 1];
-    }
-
     int[] right = new int[len];
-    IntStream.range(0, len).forEach(i -> right[i] = 1);
+    IntStream.range(0, len).forEach(i -> {
+      left[i] = 1;
+      right[i] = 1;
+    });
 
-    for (int i = 1; i < len; i++) {
-      right[len - i - 1] = right[len - i] * ary[len - i];
+    for (int i = 0; i < len - 1; i++) {
+      left[i + 1] = left[i] * ary[i];
+      right[len - i - 2] = right[len - i - 1] * ary[len - i - 1];
     }
-
-    int[] result = new int[len];
-    IntStream.range(0, len).forEach(i -> result[i] = 1);
 
     for (int i = 0; i < len; i++) {
-      result[i] = left[i] * right[i];
+      left[i] = left[i] * right[i];
     }
 
-    return result;
+    return left;
   }
 
   public static void main(String... args) {
