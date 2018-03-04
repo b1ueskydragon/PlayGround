71965e843d5fa6efb859f6b87fd5fca4dfc6ca9c
inagon <dragoalie@gmail.com>
2018-03-04 10:04:44 +0900

    180304

---
+++ src/quiz2018/EnumChallenge.java
@@ -0,0 +1,31 @@
+package quiz2018;
+
+public class EnumChallenge {
+  public static void main(String... args) {
+    DragonBalls dragonBall1 = DragonBalls.FIFTH;
+    DragonBalls dragonBall2 = DragonBalls.SECOND;
+
+    DragonBalls dragonBalls[] = DragonBalls.FIRST
+        .SECOND.THIRD.FOURTH.SIXTH.invokeShenLong();
+
+    for (DragonBalls dragonBall : dragonBalls) {
+      if (dragonBall1.equals(dragonBall)) break;
+      dragonBall2 = dragonBall1;
+    }
+    System.out.println((dragonBall2 == dragonBall1) ? "Same dragon ball" : "new Dragonball");
+    System.out.println(dragonBall2);
+  }
+
+  enum DragonBalls implements Invoker {
+    FIRST, SECOND, THIRD, FOURTH, FIFTH, SIXTH, SEVENTH;
+
+    @Override
+    public DragonBalls[] invokeShenLong() {
+      return DragonBalls.values();
+    }
+  }
+
+  interface Invoker {
+    DragonBalls[] invokeShenLong();
+  }
+}
