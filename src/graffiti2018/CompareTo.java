76799de90b1fd602293014d5fb2a150c25681919
inagon <dragoalie@gmail.com>
2018-03-26 09:42:51 +0900

    String date compare

--- src/graffiti2018/CompareTo.java
+++ src/graffiti2018/CompareTo.java
@@ -9,5 +9,13 @@ public class CompareTo {
     String o1 = "b";
     String o2 = "a";
     System.out.println(o1.compareTo(o2)); // o1 > o2 => 1
+
+    String d1 = "20180326";
+    String d2 = "201803260900";
+    System.out.println(d1.compareTo(d2)); // -4
+
+    String dd1 = "20180327";
+    String dd2 = "201803260900";
+    System.out.println(dd1.compareTo(dd2)); // 1
   }
 }
