60a86df8a48f88691f449ac6df834b1c55788ac7
jang <jang_inhwa@microad.co.jp>
2018-05-15 17:27:49 +0900

    First approach

---
+++ src/dailyOne/P73/P73.java
@@ -0,0 +1,27 @@
+package dailyOne.P73;
+
+import java.util.LinkedList;
+
+// Given the head of a singly linked list, reverse it in-place.
+public class P73 {
+  public static void main(String... args) {
+
+    LinkedList<String> lst = new LinkedList<>();
+    lst.add("A");
+    lst.add("B");
+    lst.add("C");
+    lst.add("D");
+
+    reverse(lst, lst.size() - 1);
+    lst.forEach(System.out::println);
+  }
+
+  private static <T> void reverse(LinkedList<T> curr, int insertPoint) {
+    if (insertPoint == 0) return;
+
+    T head = curr.getFirst();
+    curr.removeFirst();
+    curr.add(insertPoint, head);
+    reverse(curr, insertPoint - 1);
+  }
+}
