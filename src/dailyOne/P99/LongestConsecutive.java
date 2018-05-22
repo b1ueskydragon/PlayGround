33c24cae10600c1f074afe5257f23002b17e9a6e
jang <jang_inhwa@microad.co.jp>
2018-05-22 14:46:34 +0900

    Initial approach (Do more faster)

--- src/dailyOne/P99/LongestConsecutive.java
+++ src/dailyOne/P99/LongestConsecutive.java
@@ -1,37 +1,29 @@
 package dailyOne.P99;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
 
 /**
- * find the length of the longest consecutive elements sequence in O(n).
+ * find the length of the longest consecutive elements sequence in O(N logN).
  */
 public class LongestConsecutive {
   public static void main(String... args) {
-    Integer[] given = {100, 4, 200, 3, 2, 101};
-    extract(given).forEach(System.out::println);
+    int[] given = {103, 104, 106, 4, 5, 7, 8, 9, 100, 2, 3, 101, 102};
+    Arrays.sort(given);
+    System.out.println(countLength(given));
   }
 
-  // TreeSet O(log n)
-  // HashSet O(1)
-  private static Set<Integer> toSet(Integer[] given) {
-    return new TreeSet<>(Arrays.asList(given));
-  }
+  private static int countLength(int[] sorted) {
+    int cnt = 1;
+    int tmp = 0;
 
-  private static Set<Integer> extract(Integer[] given) {
-    final Set<Integer> table = toSet(given);
-
-    Set<Integer> tmpA = new HashSet<>();
-    Set<Integer> tmpB = new HashSet<>();
-
-    for (int i = 1; i < max ; i++) {
-      if (table.contains(i + 1) && table.contains(i - 1)) {
-        tmpA.add(i);
+    for (int i = 1; i < sorted.length; i++) {
+      if (sorted[i] - sorted[i - 1] == 1) {
+        cnt += 1;
+      } else {
+        tmp = Math.max(tmp, cnt);
+        cnt = 1;
       }
     }
-
-    return tmpA;
+    return Math.max(tmp, cnt);
   }
 }
