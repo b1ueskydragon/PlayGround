ed1f51bea4933621b9702b444fe80ae0bbfa06d4
jang <jang_inhwa@microad.co.jp>
2018-05-25 18:22:36 +0900

    Change data structure to Tuple

--- src/quiz99/P28.java
+++ src/quiz99/P28.java
@@ -1,65 +1,99 @@
 package quiz99;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class P28 {
   public static void main(String... args) {
-    final List<List<String>> given = new ArrayList<>() {{
-      add(new ArrayList<>() {{
-            add("a");
-            add("b");
-            add("c");
-          }}
-      );
-      add(new ArrayList<>() {{
-            add("d");
-            add("e");
-          }}
-      );
-      add(new ArrayList<>() {{
-            add("f");
-            add("g");
-            add("h");
-          }}
-      );
-      add(new ArrayList<>() {{
-            add("d");
-            add("e");
-          }}
-      );
-      add(new ArrayList<>() {{
-            add("i");
-            add("j");
-            add("j");
-            add("k");
-            add("l");
-          }}
-      );
-      add(new ArrayList<>() {{
-            add("m");
-            add("n");
-          }}
-      );
-      add(new ArrayList<>() {{
-            add("o");
-          }}
-      );
-    }};
-
-    lsort(given).forEach(System.out::println);
+    lsort(Given.lists).forEach(p -> System.out.println(p._1()));
   }
 
   /**
    * Sort by each length.
    *
-   * @param <T>      Type
-   * @param listList given
+   * @param <T>   Type
+   * @param lists given
    * @return return a new list (sorted)
    */
-  private static <T> List<HashMap<T, Integer>> lsort(List<List<T>> listList) {
-    return listList.stream().map(ls -> new HashMap<T, Integer>() {{put((T)ls, ls.size());}}).collect(Collectors.toList());
+  private static <T> List<Tuple<List<T>, Integer>> lsort(List<List<T>> lists) {
+
+    List<Tuple<List<T>, Integer>> pairs = new ArrayList<>();
+
+    for (List<T> list : lists) {
+      pairs.add(new Tuple<>(list, list.size()));
+    }
+
+    return pairs;
   }
+}
+
+/**
+ * Pairs
+ *
+ * @param <T> Type 1
+ * @param <V> Type 2
+ */
+class Tuple<T, V> {
+  private final T t;
+  private final V v;
+
+  Tuple(T t, V v) {
+    this.t = t;
+    this.v = v;
+  }
+
+  public T _1() {
+    return t;
+  }
+
+  public V _2() {
+    return v;
+  }
+}
+
+/**
+ * Sample datum
+ */
+class Given {
+  static final List<List<String>> lists = new ArrayList<>() {{
+    add(new ArrayList<>() {{
+          add("a");
+          add("b");
+          add("c");
+        }}
+    );
+    add(new ArrayList<>() {{
+          add("d");
+          add("e");
+        }}
+    );
+    add(new ArrayList<>() {{
+          add("f");
+          add("g");
+          add("h");
+        }}
+    );
+    add(new ArrayList<>() {{
+          add("d");
+          add("e");
+        }}
+    );
+    add(new ArrayList<>() {{
+          add("i");
+          add("j");
+          add("j");
+          add("k");
+          add("l");
+        }}
+    );
+    add(new ArrayList<>() {{
+          add("m");
+          add("n");
+        }}
+    );
+    add(new ArrayList<>() {{
+          add("o");
+        }}
+    );
+  }};
 }
