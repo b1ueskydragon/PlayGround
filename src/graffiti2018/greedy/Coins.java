af6eb3525fce3e86ec95d64e8374ece027c7f37b
jang <jang_inhwa@microad.co.jp>
2018-06-15 01:21:14 +0900

    How 'many' coins needed

--- src/graffiti2018/greedy/Coins.java
+++ src/graffiti2018/greedy/Coins.java
@@ -2,19 +2,29 @@ package graffiti2018.greedy;
 
 import java.util.Scanner;
 
-// できる限り少ない硬化で支払う.
+// できる限り少ない枚数で支払う. 枚数を求める.
 public class Coins {
+
   public static void main(String... args) {
-    coins(new Scanner(System.in).nextInt());
+    Scanner input = new Scanner(System.in);
+
+    final int[] units = {1, 5, 10, 50, 100, 500}; // 単位
+    int[] given = new int[units.length]; // 各単位の手元の枚数
+
+    for (int i = 0; i < given.length; i++) {
+      given[i] = input.nextInt();
+    }
+
+    int sum = input.nextInt();
+
+    coins(units, given, sum);
   }
 
-  private static void coins(int sum) {
-    final int[] coins = {1, 5, 10, 50, 100, 500};
-
+  private static void coins(int[] units, int[] given, int sum) {
     int res = 0;
-    for (int i = coins.length - 1; i >= 0; i--) {
-      int cnt = (int) Math.ceil(sum / coins[i]); // count of using coins[i]
-      sum -= coins[i] * cnt;
+    for (int i = units.length - 1; i >= 0; i--) {
+      int cnt = Math.min(sum / units[i], given[i]); // units[i] を使う枚数
+      sum -= units[i] * cnt;
       res += cnt;
     }
 
