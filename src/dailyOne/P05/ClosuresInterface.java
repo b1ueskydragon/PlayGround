56ae7c0a715ef21c4fe53b137423c6b292d324fe
jang <jang_inhwa@microad.co.jp>
2018-08-24 12:42:44 +0900

    Make pair class to custom interface

---
+++ src/dailyOne/P05/ClosuresInterface.java
@@ -0,0 +1,40 @@
+package dailyOne.P05;
+
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class ClosuresInterface<T, U> {
+
+  private interface Pair<T, U> {
+    T getT();
+
+    U getU();
+  }
+
+  private BiFunction<T, U, Pair> cons = (t, u) -> new Pair() {
+    @Override
+    public T getT() {
+      return t;
+    }
+
+    @Override
+    public U getU() {
+      return u;
+    }
+  };
+
+  private Function<Pair<T, U>, T> car = Pair::getT;
+
+  private Function<Pair<T, U>, U> cdr = Pair::getU;
+
+  public static void main(String... args) {
+    ClosuresInterface<String, Integer> closures = new ClosuresInterface<>();
+    Pair aPair = closures.cons.apply("a", 0);
+    String t = closures.car.apply(aPair);
+    Integer u = closures.cdr.apply(aPair);
+
+    System.out.println(t);
+    System.out.println(u);
+  }
+}
