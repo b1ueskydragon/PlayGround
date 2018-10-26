84621883bf70eb5ca079233ee683b8babf907a28
jang <jang_inhwa@microad.co.jp>
2018-10-26 12:05:41 +0900

    Get and print out max range of numeric value only

---
+++ src/graffiti2018/NumericRange.java
@@ -0,0 +1,8 @@
+package graffiti2018;
+
+public class NumericRange {
+  public static void main(String... args) {
+    System.out.println(Integer.MAX_VALUE);
+    System.out.println(Long.MAX_VALUE); // `Long.MaxValue` at scala.
+  }
+}
