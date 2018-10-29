fb756443e0ebd6da8ed9d3949e57e5cb83b827e4
jang <jang_inhwa@microad.co.jp>
2018-10-29 17:45:02 +0900

    Print max value of unsigned int

--- src/graffiti2018/NumericRange.java
+++ src/graffiti2018/NumericRange.java
@@ -3,6 +3,17 @@ package graffiti2018;
 public class NumericRange {
   public static void main(String... args) {
     System.out.println(Integer.MAX_VALUE);
-    System.out.println(Long.MAX_VALUE); // `Long.MaxValue` at scala.
+    System.out.println(Long.MAX_VALUE); // scala> Long.MaxValue
+
+    Long maxValL = (Long.MAX_VALUE >> (2 * 16)) + 1;
+
+    System.out.println(maxValL - 1 == Integer.MAX_VALUE);
+
+    System.out.println(0xffffffffL);
+
+    System.out.println((Integer.MAX_VALUE + 1) & 0xFFFFFFFFL);
+
+    // (Long.MaxValue >> (2 * 16)) + 1 == 2147483648
+    // ((Long.MaxValue >> (2 * 16)) * 2) + 1 == 4294967295. max value of unsigned int
   }
 }
