f75c7d973b9766def6a31fa8b072391f1c909288
jang <dragoalie@gmail.com>
2019-04-13 17:58:45 +0900

    wip - replace set to map and initial impl

--- src/graffiti2019/LRUCache.java
+++ src/graffiti2019/LRUCache.java
@@ -2,17 +2,51 @@ package graffiti2019;
 
 import java.util.*;
 
+// TODO: generic type
 public class LRUCache {
-  // TODO: generic type
-
-  static Deque<Integer> dq; // store keys of cache
-  static HashSet<Integer> map; // store references of key in cache
-  static int csize; // maximum capacity of cache
+  private static Deque<Integer> queue; // store keys of cache
+  private static HashMap<Integer, Integer> map; // store references of key in cache
+  private static int capacity; // maximum capacity of cache
 
   LRUCache(int n) {
-    dq = new LinkedList<>();
-    map = new HashSet<>();
-    csize = n;
+    queue = new LinkedList<>();
+    map = new HashMap<>();
+    capacity = n;
+  }
+
+  /**
+   * Sets the value of the key x with value y
+   * If the cache reaches its capacity,
+   * invalidate the least recently map item
+   *
+   * @param x key
+   * @param y value
+   */
+  void set(int x, int y) {
+    // invalidate the least recently map item
+    if (map.size() == capacity) for (int key : queue) if (!map.containsKey(key)) queue.remove(x);
+
+    queue.addFirst(x); // inserting the new item at the head
+    capacity++;
+
+    map.put(x, y);
+  }
+
+  /**
+   * Reference and get item
+   *
+   * @param x key
+   * @return the value of the x if present else -1
+   */
+  int get(int x) {
+    if (map.containsKey(x)) {
+      queue.add(x); // store references of key at the end
+
+      capacity--;
+      return map.get(x);
+    }
+
+    return -1; // not present
   }
 
 }
---
+++ src/graffiti2019/LRUCacheApp.java
@@ -0,0 +1,7 @@
+package graffiti2019;
+
+public class LRUCacheApp {
+  public static void main(String... args) {
+
+  }
+}
