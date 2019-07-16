9e38c1fc98bf07bac4947afa072a34773f5f2e3b
ina <dragoalie@gmail.com>
2019-07-17 00:01:00 +0900

    Add power set test

---
+++ src/main/java/graffiti2019/SubSet.java
@@ -0,0 +1,16 @@
+package graffiti2019;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class SubSet {
+
+  static <T> List<List<T>> generate(List<T> origin) {
+    List<List<T>> ps = new ArrayList<>();
+
+    /* add code with nested iteration (ps is mutable) */
+
+    return ps;
+  }
+
+}
---
+++ src/test/java/graffiti2019/SubSetSpec.java
@@ -0,0 +1,27 @@
+package graffiti2019;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SubSetSpec {
+
+  @Test
+  void testCharList() {
+    var expected = List.of(
+        List.of(),
+        List.of('a'),
+        List.of('b'),
+        List.of('a', 'b'),
+        List.of('c'),
+        List.of('a', 'c'),
+        List.of('b', 'c'),
+        List.of('a', 'b', 'c')
+    );
+    var actual = SubSet.generate(List.of('a', 'b', 'c'));
+    assertEquals(actual, expected);
+  }
+
+}
