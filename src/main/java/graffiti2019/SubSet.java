29fe3282868557dfca73ebc1af4414176e1b708c
ina <dragoalie@gmail.com>
2019-07-23 20:32:24 +0900

    Replace pow to shift bit

--- src/main/java/graffiti2019/SubSet.java
+++ src/main/java/graffiti2019/SubSet.java
@@ -7,12 +7,12 @@ class SubSet {
   /** Bit-op */
   static <T> List<List<T>> generate(List<T> xs) {
     var ps = new ArrayList<List<T>>();
-    var N = xs.size();
-    var resN = (long) Math.pow(2, N);
+    var len = xs.size();
+    long breadth = 2 << (len - 1);
 
-    for (var i = 0; i < resN; i++) {   // [Ø, a, b, ab, c, ac, bc, abc]
+    for (var i = 0; i < breadth; i++) {
       var chunk = new ArrayList<T>();  // init 2^N times
-      for (var j = 0; j < N; j++) {    // [a, b, c]
+      for (var j = 0; j < len; j++) {
         if ((i & (1 << j)) > 0) chunk.add(xs.get(j));
       }
       ps.add(chunk);
