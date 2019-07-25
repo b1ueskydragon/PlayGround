9af3b1f7d5aa38af74c095a4a76d58a982a67060
ina <dragoalie@gmail.com>
2019-07-26 00:00:42 +0900

    Add test set a DFS order that append first

--- src/test/java/graffiti2019/SubSetSpec.java
+++ src/test/java/graffiti2019/SubSetSpec.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static graffiti2019.SubSet.generateWithBitwise;
-import static graffiti2019.SubSet.generateWithDq;
+import static graffiti2019.SubSet.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SubSetSpec {
@@ -26,7 +25,6 @@ class SubSetSpec {
     assertEquals(expected, actual);
   }
 
-
   @Test
   void test_generate_BFS_bit_op() {
     var expected = List.of(
@@ -40,6 +38,22 @@ class SubSetSpec {
         List.of('a', 'b', 'c')
     );
     var actual = generateWithBitwise(List.of('a', 'b', 'c'));
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void test_generate_recursion() {
+    var expected = List.of(
+        List.of('a', 'b', 'c'),
+        List.of('a', 'b'),
+        List.of('a', 'c'),
+        List.of('a'),
+        List.of('b', 'c'),
+        List.of('b'),
+        List.of('c'),
+        List.of()
+    );
+    var actual = generateWithDfs(List.of('a', 'b', 'c'));
     assertEquals(expected, actual);
   }
 
