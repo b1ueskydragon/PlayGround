ed80470ba6c3bde1ba9306a64a79e93dc2553df7
ina <dragoalie@gmail.com>
2019-09-06 13:50:44 +0900

    make to Component, add TODO

--- src/test/java/factory/TestObjectFactory.java
+++ src/test/java/factory/TestObjectFactory.java
@@ -1,6 +1,9 @@
 package factory;
 
-public class TestObjectFactory {
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestObjectFactory {  // TODO to use, Autowired
 
   public TestObject create(int id, String status) {
     return new TestObject(id, status);
