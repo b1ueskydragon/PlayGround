67382c684da5604ed2825b81ae6fe341081725eb
ina <dragoalie@gmail.com>
2019-09-23 00:01:49 +0900

    Replace loop to parameterized test

--- src/test/java/leetcode/p0524/Solution01Spec.java
+++ src/test/java/leetcode/p0524/Solution01Spec.java
@@ -1,7 +1,10 @@
 package leetcode.p0524;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -12,26 +15,32 @@ public class Solution01Spec {
 
   private final Solution01 underTest = new Solution01();
 
-//  @Test
-//  public void testInit() {
-//    var s = "abcde"; // abcde is correspond to 01234
-//    Solution01.init(s); // side effect
-//
-//    int[][] expected = {
-//        {0, 1, 2, 3, 4, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
-//        {-1, 1, 2, 3, 4, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
-//        {-1, -1, 2, 3, 4, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
-//        {-1, -1, -1, 3, 4, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
-//        {-1, -1, -1, -1, 4, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
-//        {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
-//    };
-//
-//    int[][] actual = Solution01.dic; // matrix
-//
-//    assertThat(actual.length, equalTo(s.length() + 1));
-//    Arrays.stream(expected).forEach(row -> assertThat(row.length, equalTo(26)));
-//    IntStream.range(0, s.length()).forEach(i -> assertThat(actual[i], equalTo(expected[i])));
-//  }
+  @ParameterizedTest
+  @MethodSource("initMatrixProvider")
+  public void testInit(int row, int[] expected) {
+    var s = "abcde"; // abcde is correspond to 01234
+
+    Solution01.init(s); // side effect
+
+    var actual = Solution01.dic;
+
+    assertThat(actual[row], equalTo(expected));
+
+    assertThat(actual.length, equalTo(s.length() + 1));
+    assertThat(actual[0].length, equalTo('z' - 'a' + 1));
+  }
+
+  private static Iterable<Object[]> initMatrixProvider() {
+    return Arrays.asList(new Object[][]{
+            {0, new int[]{0, 1, 2, 3, 4, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}},
+            {1, new int[]{-1, 1, 2, 3, 4, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}},
+            {2, new int[]{-1, -1, 2, 3, 4, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}},
+            {3, new int[]{-1, -1, -1, 3, 4, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}},
+            {4, new int[]{-1, -1, -1, -1, 4, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}},
+            {5, new int[]{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}}
+        }
+    );
+  }
 
   @Test
   public void testFindLongestWord() {
