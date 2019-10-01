a49b898711fcc98ccc4031b1a9675621615bf9f6
ina <dragoalie@gmail.com>
2019-10-01 12:12:13 +0900

    Minor fix

--- src/test/java/graffiti2019/jsons/MyNotificationSpec.java
+++ src/test/java/graffiti2019/jsons/MyNotificationSpec.java
@@ -8,20 +8,20 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MyNotificationSpec {
+public class MyNotificationSpec {
 
   private ObjectMapper mapper;
 
   private MyNotification underTest;
 
   @BeforeEach
-  void setUp() throws Exception {
+  public void setUp() throws Exception {
     mapper = new ObjectMapper().configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
     underTest = new MyNotification("Shisa", 101, null);
   }
 
   @Test
-  void test_set_include_nonNull() throws Exception {
+  public void testSetIncludeNonNull() throws Exception {
     mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
     var expected = "{\"name\":\"Shisa\",\"contentId\":101}";
     var actual = mapper.writeValueAsString(underTest);
@@ -29,7 +29,7 @@ class MyNotificationSpec {
   }
 
   @Test
-  void test_disable_include_nonNull() throws Exception {
+  public void testDisableIncludeNonNull() throws Exception {
     var expected = "{\"name\":\"Shisa\",\"contentId\":101,\"errors\":null}";
     var actual = mapper.writeValueAsString(underTest);
     assertEquals(expected, actual);
