2de17b1e2211ee0cc6e49948fd0b677d04993dfc
ina <dragoalie@gmail.com>
2019-10-05 01:14:55 +0900

    Folding with java

---
+++ src/main/java/graffiti2019/FoldLeft.java
@@ -0,0 +1,39 @@
+package graffiti2019;
+
+
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+// TODO Spec
+public class FoldLeft {
+
+  static class Basic {
+
+    public static <T, U> U foldLeft(List<T> seq, U acc, BiFunction<U, T, U> f) {
+      if (seq.isEmpty()) {
+        return acc;
+      } else {
+        return foldLeft(seq.subList(1, seq.size()), f.apply(acc, seq.get(0)), f);
+      }
+    }
+
+  }
+
+  static class WithCollectors {
+
+    public static <T, U> Collector<T, ?, U> foldLeft(
+        final U init,
+        final BiFunction<? super U, ? super T, ? extends U> f
+    ) {
+      return Collectors.collectingAndThen(
+          Collectors.reducing(Function.<U>identity(), t -> u -> f.apply(u, t), Function::andThen),
+          acc -> acc.apply(init)
+      );
+    }
+
+  }
+
+}
