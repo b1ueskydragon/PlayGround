b734661b76f9e49e946991dcd49b01542a074418
ina <dragoalie@gmail.com>
2019-10-20 03:11:40 +0900

    left-first dfs with go deeper with while-loop

--- src/test/java/leetcode/p0114/SolutionSpec.java
+++ src/test/java/leetcode/p0114/SolutionSpec.java
@@ -8,6 +8,8 @@ import static org.junit.Assert.assertThat;
 public class SolutionSpec {
 
   private final Solution underTest = new Solution();
+  private final Solution01 underTestB = new Solution01();
+
 
   @Test
   public void testFlatten() {
@@ -19,6 +21,30 @@ public class SolutionSpec {
     root.right.right = new TreeNode(6);
 
     underTest.flatten(root);
+
+    assertThat(1, equalTo(root.val));
+    assertThat(null, equalTo(root.left));
+    assertThat(2, equalTo(root.right.val));
+    assertThat(null, equalTo(root.right.left));
+    assertThat(3, equalTo(root.right.right.val));
+    assertThat(null, equalTo(root.right.right.left));
+    assertThat(4, equalTo(root.right.right.right.val));
+    assertThat(null, equalTo(root.right.right.right.left));
+    assertThat(5, equalTo(root.right.right.right.right.val));
+    assertThat(null, equalTo(root.right.right.right.right.left));
+    assertThat(6, equalTo(root.right.right.right.right.right.val));
+  }
+
+  @Test
+  public void testFlattenB() {
+    final var root = new TreeNode(1);
+    root.left = new TreeNode(2);
+    root.right = new TreeNode(5);
+    root.left.left = new TreeNode(3);
+    root.left.right = new TreeNode(4);
+    root.right.right = new TreeNode(6);
+
+    underTestB.flatten(root);
 
     assertThat(1, equalTo(root.val));
     assertThat(null, equalTo(root.left));
