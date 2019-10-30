7b67c9d7cae53e0fa72e28e8d3133a8246cf9eb9
ina <dragoalie@gmail.com>
2019-10-30 09:11:04 +0900

    Rename test case

--- src/test/java/graffiti2019/HiFastutilsSpec.java
+++ src/test/java/graffiti2019/HiFastutilsSpec.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertThat;
 public class HiFastutilsSpec {
 
   @Test
-  public void test_justGen() {
+  public void shouldProductScalarFromSameDimensionVectors() {
     final int[] tx = {1, 3, 5, 7, 9};
     final double[] x = {0.1, 0.3, 0.5, 0.7, 0.9};
     assertThat(HiFastutils.productScalar(tx, x), Matchers.is(closeTo(16.5, 1e-6)));
