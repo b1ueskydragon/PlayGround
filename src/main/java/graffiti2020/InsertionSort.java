6d7bc7aff2db74a5cace7953064320bf13b88250
ina <dragoalie@gmail.com>
2020-01-17 18:21:00 +0900

    Trying to use sentinel with min int

--- src/main/java/graffiti2020/InsertionSort.java
+++ src/main/java/graffiti2020/InsertionSort.java
@@ -2,7 +2,6 @@ package graffiti2020;
 
 class InsertionSort {
   static int[] sortAscInplace(final int[] nums) {
-    // TODO sentinel?
     for (int i = 1; i < nums.length; i++) {
       final int fixedTmp = nums[i];
       int j = i;
@@ -11,6 +10,19 @@ class InsertionSort {
         j--;
       }
       nums[j] = fixedTmp; // put it on the next of j-1 (== j)
+    }
+    return nums;
+  }
+
+  static int[] sortAscInplaceHasASentinel(final int[] nums) {
+    for (int i = 2; i < nums.length; i++) {
+      final int fixedTmp = nums[i];
+      int j = i;
+      while (nums[j - 1] > fixedTmp) {
+        nums[j] = nums[j - 1];
+        j--;
+      }
+      nums[j] = fixedTmp;
     }
     return nums;
   }
--- src/test/java/graffiti2020/InsertionSortSpec.java
+++ src/test/java/graffiti2020/InsertionSortSpec.java
@@ -9,10 +9,26 @@ import static org.junit.Assert.assertThat;
 
 class InsertionSortSpec {
   @Test
+  void testDebug() {
+    final int[] xs = {5, 4, 1, 2, 3};
+    final int[] expected = xs.clone();
+    Arrays.sort(expected);
+    assertThat(InsertionSort.sortAscInplace(xs), equalTo(expected));
+  }
+
+  @Test
   void testInsertionSort() {
     final int[] xs = {11, 3, 6, 4, 4, 12, 1, 2, 3};
     final int[] expected = xs.clone();
     Arrays.sort(expected);
     assertThat(InsertionSort.sortAscInplace(xs), equalTo(expected));
+  }
+
+  @Test
+  void testInsertionSortHasASentinel() {
+    final int[] xs = {Integer.MIN_VALUE, 11, 3, 6, 4, 4, 12, 1, 2, 3};
+    final int[] expected = xs.clone();
+    Arrays.sort(expected);
+    assertThat(InsertionSort.sortAscInplaceHasASentinel(xs), equalTo(expected));
   }
 }
