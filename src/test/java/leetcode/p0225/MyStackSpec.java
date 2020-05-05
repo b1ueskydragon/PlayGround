9981daf89d9ea708225cf465311e5f7b84deabf4
ina <dragoalie@gmail.com>
2020-05-05 10:45:43 +0900

    idea - implement a stack with two queues

---
+++ src/test/java/leetcode/p0225/MyStackSpec.java
@@ -0,0 +1,31 @@
+package leetcode.p0225;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyStackSpec {
+  @Test
+  void testPushAndPop() {
+    final MyStack myStack = new MyStack();
+    myStack.push(1);
+    myStack.push(2);
+    assertEquals(2, myStack.top());
+    assertEquals(2, myStack.pop());
+    assertFalse(myStack.empty());
+  }
+
+  @Test
+  void testPushAfterPop() {
+    final MyStack myStack = new MyStack();
+    myStack.push(1);
+    myStack.push(2);
+    assertEquals(2, myStack.top());
+    assertEquals(2, myStack.pop());
+    myStack.push(3);
+    assertEquals(3, myStack.pop());
+    assertEquals(1, myStack.pop());
+    assertTrue(myStack.empty());
+  }
+
+}
