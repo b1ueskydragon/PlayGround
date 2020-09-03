8cf21ab3305422ee8f970acab16828241dbf7bf1
ina <dragoalie@gmail.com>
2020-09-03 15:03:02 +0900

    solved? - O(N) time to find the smallest pos-integer cannot be built up subsets

--- src/main/java/dailyOne/p641/Solution.java
+++ src/main/java/dailyOne/p641/Solution.java
@@ -7,14 +7,18 @@ public class Solution {
     }
     int lim = 0;
     int prev = 0;
+    int sumSoFar = 0;
 
     for (int x : sortedArr) {
-      if (x == 1) prev++; // since next num could be built to do +1.
+      if (x == 1 || sumSoFar > x) prev++; // since next num could be built to do +1.
       else if (x - prev == 1) prev = x;
       else break;
 
+      sumSoFar += x;
       lim++;
     }
+
+    System.out.println(sumSoFar);
 
     int res = 1;
     for (int i = 0; i < lim; i++) {
