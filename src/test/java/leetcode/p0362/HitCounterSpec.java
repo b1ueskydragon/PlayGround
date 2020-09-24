e59bbf4833ff461cd20619da26be28f711e07a23
ina <dragoalie@gmail.com>
2020-09-24 15:05:53 +0900

    improve - using queue to save memory space

---
+++ src/test/java/leetcode/p0362/HitCounterSpec.java
@@ -0,0 +1,44 @@
+package leetcode.p0362;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class HitCounterSpec {
+
+  @Test
+  public void test1to4() {
+    HitCounter counter = new HitCounter();
+
+    counter.hit(1);
+    counter.hit(2);
+    counter.hit(3);
+
+    assertThat(counter.getHits(4), equalTo(3));
+  }
+
+  @Test
+  public void test1to300() {
+    HitCounter counter = new HitCounter();
+
+    counter.hit(1);
+    counter.hit(2);
+    counter.hit(3);
+    counter.hit(300);
+
+    assertThat(counter.getHits(300), equalTo(4));
+  }
+
+  @Test
+  public void test2to301() {
+    HitCounter counter = new HitCounter();
+
+    counter.hit(1);
+    counter.hit(2);
+    counter.hit(3);
+    counter.hit(300);
+
+    assertThat(counter.getHits(301), equalTo(3));
+  }
+}
