2b4289c3dc0394759df9b49f55aaa7c83b46b1ae
ina <dragoalie@gmail.com>
2020-11-11 11:20:53 +0900

    solved - find a fixed point in sorted array of distinct nums. use Binary search

---
+++ src/main/java/dailyOne/p708/Solution.java
@@ -0,0 +1,27 @@
+package dailyOne.p708;
+
+public class Solution {
+
+  public int fixedPoint(final int[] sortedNums) {
+    int l = 0;
+    int r = sortedNums.length - 1;
+
+    while (l <= r) {
+      int m = l + (r - l) / 2;
+      int maybeFixedPoint = sortedNums[m];
+
+      if (m == maybeFixedPoint) {
+        return m;
+      }
+
+      if (m > maybeFixedPoint) {
+        l = m + 1;
+      } else {
+        r = m - 1;
+      }
+    }
+
+    throw new IllegalArgumentException("Not found.");
+  }
+
+}
